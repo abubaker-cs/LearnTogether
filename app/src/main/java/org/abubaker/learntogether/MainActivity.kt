26625cbf221e4d5568dabb93a1f1e25dd7c68f09
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2024-02-24 00:05:19 +0200

    Challenge completed

--- app/src/main/java/org/abubaker/learntogether/MainActivity.kt
+++ app/src/main/java/org/abubaker/learntogether/MainActivity.kt
@@ -1,15 +1,24 @@
 package org.abubaker.learntogether
 
 import android.os.Bundle
+import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.abubaker.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,69 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PostArticle()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PostArticle() {
+
+    PostCard(
+        postTitle = stringResource(R.string.post_title),
+        postDesc = stringResource(R.string.post_desc),
+        postContent = stringResource(R.string.post_content),
+        postThumbnail = painterResource(R.drawable.bg_compose_background)
     )
+
 }
 
-@Preview(showBackground = true)
+@Composable
+fun PostCard(
+    postTitle: String,
+    postDesc: String,
+    postContent: String,
+    postThumbnail: Painter
+) {
+
+    Column {
+        Image(
+            painter = postThumbnail,
+            contentDescription = null
+        )
+
+        Text(
+            text = postTitle,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+
+        Text(
+            text = postDesc,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = postContent,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+
+
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun GreetingPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        PostArticle()
     }
 }
